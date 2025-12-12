81c04f66e529c670a3d85b1820b4b0015bccc8f1
Himanshuu <2203himanshusingh@gmail.com>
2025-12-12 21:55:56 +0530

    first commit -2

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import "../style/SignUp.css";
+import "../style/Signup.css";
 import { AuthContext } from "../context/AuthContext";
 
 const Signup = () => {
