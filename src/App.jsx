286641875b7d6efdfe90248b7516e9d22a650900
Himanshuu <2203himanshusingh@gmail.com>
2025-12-27 01:48:50 +0530

    updating app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Profile from "./page/Profile";
 import Cart from "./page/Cart";
 import ProductDesc from "./page/ProductDesc";
 import ForgotPassword from "./page/ForgetPassword";
+import Favourite from "./page/Favourite";
 
 import Layout from "./component/Layout";
 
@@ -82,6 +83,7 @@ const App = () => {
                       path="/forgot-password"
                       element={<ForgotPassword />}
                     />
+                    <Route path="/favourite" element={<Favourite />} />
 
                     {/* USER PROTECTED */}
                     <Route
